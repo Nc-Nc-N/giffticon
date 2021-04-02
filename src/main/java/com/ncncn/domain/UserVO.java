eb8421b265fb381fc25c5461a74d9f885847195c
planner26 <planner263@gmail.com>
2021-04-02 20:10:25 +0900

    UserVO 수정

--- src/main/java/com/ncncn/domain/UserVO.java
+++ src/main/java/com/ncncn/domain/UserVO.java
@@ -1,2 +1,24 @@
-package com.ncncn.domain;public class UserVO {
+package com.ncncn.domain;
+
+public class UserVO {
+
+    private int id;
+
+    private String email;
+
+    private String pwd;
+
+    private String name;
+
+    private String telNo;
+
+    private int pnt;
+
+    private String memo;
+
+    private String emlAuthTkn;
+
+    private String stusCode;
+
+    private String roleCode;
 }
