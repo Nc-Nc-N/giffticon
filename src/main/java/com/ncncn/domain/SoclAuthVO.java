5f27a7400f160c6aad1051fc45f82510769e9d0a
HYE_JI <leehj.kk@gmail.com>
2021-04-02 20:04:34 +0900

    initial-setting# add domain

---
+++ src/main/java/com/ncncn/domain/SoclAuthVO.java
@@ -0,0 +1,16 @@
+package com.ncncn.domain;
+
+import lombok.Data;
+
+@Data
+public class SoclAuthVO {
+
+	private int userId;
+	private String soclTypeCode;
+	private String tknType;
+	private String accesTkn;
+	private int accesTknExpirIn;
+	private String rfrshTkn;
+	private int rfrshTknExpirIn;
+
+}
---
+++ src/main/java/com/ncncn/domain/TermsVO.java
@@ -0,0 +1,11 @@
+package com.ncncn.domain;
+
+import lombok.Data;
+
+@Data
+public class TermsVO {
+
+	private int id;
+	private String cntnt;
+
+}
