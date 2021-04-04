5eca76135db15b6174d89d3f0282d8760046a43f
HYE_JI <leehj.kk@gmail.com>
2021-04-04 14:27:02 +0900

    test setting

---
+++ src/main/java/com/ncncn/controller/SampleController.java
@@ -0,0 +1,18 @@
+package com.ncncn.controller;
+
+import lombok.extern.log4j.Log4j;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sample/*")
+@Log4j
+public class SampleController {
+
+	@GetMapping("/all")
+	public void all() {
+		log.info("do all");
+	}
+}
