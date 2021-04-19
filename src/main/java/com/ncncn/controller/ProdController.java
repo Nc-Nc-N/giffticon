577a8b5c6e1546999e530d2c115f289e9f79ea98
heeyoung2da <mnkl1225@gmail.com>
2021-04-19 10:42:39 +0900

    controller import 수정

--- src/main/java/com/ncncn/controller/ProdController.java
+++ src/main/java/com/ncncn/controller/ProdController.java
@@ -11,11 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import sun.tools.jconsole.JConsole;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Controller
 @Log4j
