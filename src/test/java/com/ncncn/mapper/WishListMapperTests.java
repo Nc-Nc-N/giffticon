e473389e52746c0a7bfab1c4650ab39f53c8d795
heeyoung2da <mnkl1225@gmail.com>
2021-04-25 22:20:26 +0900

    test code 추가

--- src/test/java/com/ncncn/mapper/WishListMapperTests.java
+++ src/test/java/com/ncncn/mapper/WishListMapperTests.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
+
 import static junit.framework.TestCase.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -17,45 +19,85 @@ import static junit.framework.TestCase.assertEquals;
 public class WishListMapperTests {
 
 	@Setter(onMethod_ = @Autowired)
-	private WishListMapper mapper;
+	private WishListMapper wishMapper;
+
+	@Setter(onMethod_ = @Autowired)
+	private GifticonMapper giftiMapper;
 
 	@Test
-	public void testCreate(){
+	public void testInsertWish(){
+
 		WishListVO wish = new WishListVO();
 
-		wish.setUserId(1);
-		wish.setProdCode("010103");
+		wish.setUserId(160);
+		wish.setProdCode("010203");
 
-		mapper.insertWish(wish);
+		int result = wishMapper.insertWish(wish);
+
+		assertEquals(1, result);
+
+		wishMapper.deleteWish(wish);
 	}
 
 	@Test
-	public void testSelect(){
+	public void testHasWish(){
+
 		WishListVO wish = new WishListVO();
 
-		wish.setUserId(156);
-		wish.setProdCode("010101");
+		wish.setUserId(160);
+		wish.setProdCode("010102");
 
-		int hasWish = mapper.hasWish(wish);
-		log.info(hasWish);
+		wishMapper.insertWish(wish);
+
+		int hasWish = wishMapper.hasWish(wish);
 
 		assertEquals(hasWish, 1);
+
+		wishMapper.deleteWish(wish);
+
+		hasWish = wishMapper.hasWish(wish);
+
+		assertEquals(hasWish, 0);
 	}
 
 	@Test
 	public void testDelete(){
+
 		WishListVO wish = new WishListVO();
 
 		wish.setUserId(1);
 		wish.setProdCode("010101");
+		wishMapper.insertWish(wish);
 
-		mapper.deleteWish(wish);
+		int result = wishMapper.deleteWish(wish);
+
+		assertEquals(1, result);
 	}
 
 	@Test
-	public void testgetWishList(){
+	public void testGetWishList(){
 
-		mapper.getWishList(156).forEach(wish -> log.info(wish));
+		int userId = 160;
+		List<WishListVO> list = wishMapper.getWishList(userId);
+		int cnt = list.size();
 
+		list.forEach(wish -> log.info(wish));
+
+		assertEquals(160, list.get(0).getUserId());
+		assertEquals("002", list.get(0).getGftStusCode());
+	}
+
+	@Test
+	public void testGetWishListWithPaging(){
+
+		int userId = 160;
+		int amount = 4;
+		int pageNum = 1;
+
+		List<WishListVO> list = wishMapper.getWishListWithPaging(userId, amount, pageNum);
+
+		list.forEach(gifti -> log.info(gifti));
+
+		assertEquals(4, list.size());
 	}
 }
